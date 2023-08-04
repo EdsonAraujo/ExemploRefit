82e428f059e4e4beaacb02c1cd5a75964d7fa8dc
EdsonAraujo <32927594+EdsonAraujo@users.noreply.github.com>
2023-08-04 18:10:24 -0300

    Add files via upload
    Biblioteca Refit

---
+++ ExemploRefit/ExemploRefit/ICepApiService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Refit;
+
+
+
+
+
+
+namespace ExemploRefit
+{
+	internal interface ICepApiService
+	{
+        [Get("/ws/{cep}/json/")]
+        Task<CepResponse> GetAddressAsync(string cep);
+	
+		
+	}
+
+}
+
+ 
---
+++ ExemploRefit/ExemploRefit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Refit;
+using System;
+
+
+namespace ExemploRefit
+{
+    class MainClass
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu cep"); 
+                
+                string cepInformado = Console.ReadLine().ToString();
+
+                Console.WriteLine("Consultando informação do CEP informando" + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);  
+
+                Console.WriteLine($"\nLogradouro: {address.Logradouro},\nBairro:{address.Bairro}, \nCidade:{address.Localidade}-{address.Uf}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Erro na consulta de cep" + e.Message);
+            }
+
+        }
+    }
+}
